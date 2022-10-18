203c0c053b8b99b21accdcbed7fed9fe0da779d3
Fernando Leal <lealp.fernando@gmail.com>
2022-10-18 11:45:38 -0700

    Login front

---
+++ app/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/home',
+    element: <Home />,
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />;
+    </>
+  );
+}
+
+export default App;
